2ce35c49b07563cd464614558f1bb937d8e16248
3laaAl6ore <alaatory90@gmail.com>
2021-11-24 21:54:52 +0200

    1.0.2

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -24,7 +24,7 @@ const SignUpScreen = (props) => {
     setTimeout(() => {
       setIsLoding(false);
       props.navigation.navigate('AppEntery')
-      }, 3000);
+      }, 1000);
        
     
   }
--- navigation.js
+++ navigation.js
@@ -9,14 +9,23 @@ import SignUpScreen from "./Screens/SignUp";
 import AppEnteryScreen from "./Screens/AppEntery";
 
 //3
+
 const Stacknavigator = createStackNavigator();
 export const Nav = () => {
   return (
     <Stacknavigator.Navigator>
-      <Stacknavigator.Screen name="AppEntery" component={AppEnteryScreen} />
       <Stacknavigator.Screen name="Home" component={HomeScreen} />
+
+      <Stacknavigator.Screen 
+      name="AppEntery" 
+      component={AppEnteryScreen} 
+      options = {{header :()=> null}}
+      />
+
       <Stacknavigator.Screen name="SignUp" component={SignUpScreen} />
-      <Stacknavigator.Screen name="Login" component={LoginScreen} />
+
+      <Stacknavigator.Screen name="Login" component={LoginScreen} 
+       />
     </Stacknavigator.Navigator>
   );
 };
