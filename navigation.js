38fb670fdaf7711093eb4ed0138d7385614a6a61
3laaAl6ore <86629313+3laaAl6ore@users.noreply.github.com>
2021-11-25 10:01:53 +0200

    1.0.5

--- navigation.js
+++ navigation.js
@@ -7,15 +7,24 @@ import HomeScreen from "./Screens/Home";
 import LoginScreen from "./Screens/Login";
 import SignUpScreen from "./Screens/SignUp";
 import AppEnteryScreen from "./Screens/AppEntery";
-
+import testScreen from "./Screens/testScreen";
 //3
 
 const Stacknavigator = createStackNavigator();
 export const Nav = () => {
   return (
     <Stacknavigator.Navigator >
-      <Stacknavigator.Screen name="Home" component={HomeScreen} />
 
+    <Stacknavigator.Screen 
+    name="testScreen" 
+    component={testScreen}
+    options = {{
+      header :()=> null,      
+    }}
+    />
+
+
+      <Stacknavigator.Screen name="Home" component={HomeScreen} />
       <Stacknavigator.Screen 
       name="AppEntery" 
       component={AppEnteryScreen} 
